802ce92e2a74ed090dacb33079111ee5d3bf2fdc
Ruan Aragão <ruanaragao2@gmail.com>
2021-10-03 15:41:32 -0300

    Add comments

--- app/src/main/java/com/example/goodluck/MainActivity.kt
+++ app/src/main/java/com/example/goodluck/MainActivity.kt
@@ -20,20 +20,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Mount Ticket Function
+     */
     private fun mountTicket() {
         val numList = generateNumber()
         val numberView: TextView = findViewById(R.id.numberView)
 
+        // Assemble the string with all the numbers and space between the third and fourth number.
         numberView.text = "${numList[0]}${numList[1]}${numList[2]} ${numList[3]}${numList[4]}${numList[5]}"
     }
 
+    /**
+     * Generate Random Number
+     */
     private fun generateNumber(): MutableList<Int> {
         val number: MutableList<Int> = ArrayList()
 
+        // Repeat for 6 times
         for (i in 1..6) {
+            // Add a random number between 0 and 9 to the array of numbers "number"
             number.add((0..9).random())
         }
 
+        // Return the array
         return number
     }
 }
